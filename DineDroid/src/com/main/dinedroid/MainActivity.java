12f1946da72e573d3e7a61e30c7ebc1b593cd55b
harshithamahendra <harshithamahendra@gmail.com>
2014-03-30 02:18:38 -0400

    test

--- DineDroid/src/com/main/dinedroid/MainActivity.java
+++ DineDroid/src/com/main/dinedroid/MainActivity.java
@@ -9,27 +9,11 @@ import com.main.dinedroid.menu.Menu;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.app.FragmentTransaction;
 import android.app.FragmentManager;
 import android.util.Log;
 import android.view.View;
 
-
-/**
- * An activity representing a list of Food Items. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
- * lead to a {@link FoodDetailActivity} representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
- * <p>
- * The activity makes heavy use of fragments. The list of items is a
- * {@link FoodListFragment} and the item details
- * (if present) is a {@link FoodDetailFragment}.
- * <p>
- * This activity also implements the required
- * {@link FoodListFragment.Callbacks} interface
- * to listen for item selections.
- */
 public class MainActivity extends FragmentActivity
 implements FoodListFragment.Callbacks {
 
@@ -44,6 +28,8 @@ implements FoodListFragment.Callbacks {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		FragmentManager fm = getFragmentManager();
+		FragmentTransaction ft = fm.beginTransaction();
+		
 
 		if (findViewById(R.id.food_detail_container) != null) {
 			// The detail container view will be present only in the
